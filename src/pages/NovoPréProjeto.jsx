593790baae99163a30a9e7180a46dd38c39cabde
Thiago Fontes <thiago@ecomareng.com>
2026-03-03 02:03:28 +0000

    File changes

--- src/pages/NovoPréProjeto.jsx
+++ src/pages/NovoPréProjeto.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
